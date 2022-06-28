daec4f8afa6543496e014a8f2091591668b48799
Jason Yu <jasonyu0722@gmail.com>
2022-06-28 10:42:14 +0800

    [Feature] Replace schedule MQ with priority queue

--- newsfeed-generation-service/server.js
+++ newsfeed-generation-service/server.js
@@ -9,13 +9,20 @@ server.listen(NFGS_PORT, async () => {
     console.log(`Listening on port: ${NFGS_PORT}`);
 
     // send 'recalculateAffinity' job to updaterJobQueue every 12 hours
+    // highest priority
     const recalcAffinity = schedule.scheduleJob("* */12 * * *", () => {
-        updaterJobQueue.add({ function: "recalcAffinityTable" });
+        updaterJobQueue.add(
+            { function: "recalcAffinityTable" },
+            { priority: 1 }
+        );
     });
 
     // send 'recalcTimeDecayFactor' job to updaterJobQueue every 5 minutes
     const recalcTimeDecayFactor = schedule.scheduleJob("*/5 * * * *", () => {
-        updaterJobQueue.add({ function: "recalcTimeDecayFactor" });
+        updaterJobQueue.add(
+            { function: "recalcTimeDecayFactor" },
+            { priority: 2 }
+        );
     });
 
     // const testJob = schedule.scheduleJob("* * * * * *", () => {
