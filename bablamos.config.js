0b06c4e9bd62b34e83dc9f060804527dbe6b0378
Jason Yu <jasonyu0722@gmail.com>
2022-07-03 16:28:58 +0800

    [Test] config.js

--- bablamos.config.js
+++ bablamos.config.js
@@ -3,27 +3,22 @@ module.exports = {
         {
             name: "web-server",
             script: "./server.js",
-            watch: true,
         },
         {
             name: "NFGS-server",
             script: "./newsfeed-generation-service/server.js",
-            watch: true,
         },
         {
             name: "popularity-calculator",
             script: "./mq-workers/popularity-calculator.js",
-            watch: true,
         },
         {
             name: "schedule-updater",
             script: "./mq-workers/schedule-updater.js",
-            watch: true,
         },
         {
             name: "socket-server",
             script: "./socket-server/server.js",
-            watch: true,
         },
     ],
 };
