f5c07c849984a11e48bedf95528d26d2dfbff1cf
Jason Yu <jasonyu0722@gmail.com>
2022-07-30 12:57:30 +0800

    [Deploy] Remove unneeded files

--- test/teardown.js
+++
@@ -1,7 +0,0 @@
-const { closeConnection } = require("./fake-data-generator");
-const { requester } = require("./setup");
-
-after(async () => {
-    await closeConnection();
-    requester.close();
-});
